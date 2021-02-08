1e780a9942332f7c5ac482bf7d96e373f7b9695a
hilmansulaeman <hilmansulaeman0504@gmail.com>
2021-02-09 06:34:51 +0700

    first commit

---
+++ pertemuan 2/pertemuan2.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Hilman Sulaeman
+203040029
+github.com/hilmansulaeman/pw2021_203040029
+pertemuan 2 (10 feb 2021 )
+Materi minggu ini mempelajari mengenai penggunaan sintaks PHP Dasar
+*/
+// ini adalah komentar
+// ini juga komentar
+
+// Standar output
+// echo, print
+// var_dump
+
+// echo "Hilman Sulaeman";
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh di awali dengan angka,tapi boleh mengandung angka
+// $nama ="Hilman Sulaeman";
+// Operator
+// aritmatika
+//  + - * / %
+// $x = 10;
+// $y = 20;
+//  echo $x * $y;
+
+// penggabung string / contatenation / concat
+// .
+// $nama_depan = "Hilman";
+// $nama_belakang = "Sulaeman"
+// echo $nama_depan ." ". $nama_belakang
+
+// Assignment
+//  =. +=, -=, *=, %=, .=
+// $x 1;
+// $x -=5;
+// echo $x;
+// $nama = "Hilman";
+// $nama .= " ";
+// $nama .= "Sulaeman";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==
+var_dump(1 == "1");
+
+// identitas
+//  ===, !==
+// var_dump(1 === "1"),
+
+//  Logika
+// &&, || , !
+$x = 30;
+// var_dump($x < 20 || $x & 2 == 0);
+
+ ?>
+ <!-- <!DOCTYPE html>
+ <html lang="en"
+ <head>
+ 	<meta charset="utf-8">
+ 	<title>Belajar PHP</title>
+ </head>
+ <body>
+ 	<h1>Halo, Selamat Datang <?php echo "Hilman"; ?><h1>
+ </body>
+ </html> -->
